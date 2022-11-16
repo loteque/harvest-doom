42eddc71ed80897f0ac86243077bae3ec46fa80a
loteque <andrewandroid@gmail.com>
2022-11-17 00:03:15 +0100

    Add placement grid to the camera system
    This patch adds a placement grid to the camera system.
    The cursor snaps to the grid.

--- project/src/Player/CameraController.gd
+++ project/src/Player/CameraController.gd
@@ -1,16 +1,19 @@
 extends Spatial
 
 # Export parameters exposing them to godots editor's inspector panel
-export (float, 0, 100) var movement_speed
+export (float, 0, 100) var movement_speed = 15.0
 export (NodePath) var _map_path
 
 # Set Variables when tree is ready
+onready var _grid_map = get_parent()
 onready var camera = get_node("CameraGimbal/Camera")
 onready var ray = get_node("CameraGimbal/RayCast")
 onready var cursor = get_node("CameraGimbal/Cursor")
 
 # Set variables at time of load
 var ray_length = 1000
+var grid_map_intersection
+var selection_grid_map_position
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -39,9 +42,17 @@ func _move(delta: float) -> void:
 	translation += velocity * delta * movement_speed
 	
 # Cursor functions
-func _cursor_follow_camera(delta: float):
+func _cursor_follow_camera(delta: float) -> void:
 	if !ray.is_colliding():
 		cursor.visible = false
 	else:
-		cursor.global_transform.origin = ray.get_collision_point()
+		#convert collison point intro gridmap coordinate
+		grid_map_intersection = _grid_map.world_to_map(ray.get_collision_point())
+		#convert gridmap coordinate into worldspace coordinate
+		selection_grid_map_position = _grid_map.map_to_world(grid_map_intersection.x, 
+																		 grid_map_intersection.y,
+																		 grid_map_intersection.z)
+		#move cursor to converted worldspace coordinte
+		cursor.global_transform.origin = selection_grid_map_position
+		#set the cursor to visible
 		cursor.visible = true
