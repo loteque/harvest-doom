bb2c22add29c2c786a2f6517125aacbd52b6a284
loteque <andrewandroid@gmail.com>
2022-11-16 01:39:31 +0100

    Add test-world
    Add a test world and the player camera so that we can figure out this raycasting stuff once and for all!

--- project/src/Player/CameraController.gd
+++ project/src/Player/CameraController.gd
@@ -2,9 +2,17 @@ extends Spatial
 
 # Export parameters exposing them to godots editor's inspector panel
 export (float, 0, 100) var movement_speed
+export (NodePath) var _map_path
 
-# Set variables time of load
+# Set Variables when tree is ready
+onready var camera = get_node("CameraController/Camera")
+onready var ray = get_node("CameraController/RayCast")
+onready var cursor = get_node("CameraController/Cursor")
 
+# Set variables at time of load
+var ray_length = 1000
+var cursor_normal
+var cursor_pos
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,6 +22,10 @@ func _ready():
 func _process(delta):
 	_move(delta)
 
+# Overrides the physics_process() function, runs for every tick of the phys engine (constant time)
+func _physics_process(delta):
+	# make a ray from one point extending in a defined direction
+	pass
 # Movement functions
 func _move(delta: float) -> void:
 	var velocity = Vector3()
