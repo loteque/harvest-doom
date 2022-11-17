7b2d8c46ff00daa8c23b252f2ca052beb5a59541
loteque <andrewandroid@gmail.com>
2022-11-17 21:38:26 +0100

    place cursor flush with ground

--- project/src/Player/CameraController.gd
+++ project/src/Player/CameraController.gd
@@ -1,7 +1,7 @@
 extends Spatial
 
 # Export parameters exposing them to godots editor's inspector panel
-export (float, 0, 100) var movement_speed = 15.0
+export (float, 0, 100) var movement_speed = 10.0
 export (NodePath) var _map_path
 
 # Set Variables when tree is ready
@@ -46,12 +46,14 @@ func _cursor_follow_camera(delta: float) -> void:
 	if !ray.is_colliding():
 		cursor.visible = false
 	else:
+		#translategrid to the coords of the ray intersection
+		_grid_map.global_transform.origin.y = ray.get_collision_point().y 
 		#convert collison point intro gridmap coordinate
 		grid_map_intersection = _grid_map.world_to_map(ray.get_collision_point())
 		#convert gridmap coordinate into worldspace coordinate
 		selection_grid_map_position = _grid_map.map_to_world(grid_map_intersection.x, 
-																		 grid_map_intersection.y,
-																		 grid_map_intersection.z)
+															 grid_map_intersection.y,
+															 grid_map_intersection.z)
 		#move cursor to converted worldspace coordinte
 		cursor.global_transform.origin = selection_grid_map_position
 		#set the cursor to visible
