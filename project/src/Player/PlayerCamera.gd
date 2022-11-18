e27e6bcd018ed247bcac0a07994f4e3f17f18eaf
loteque <andrewandroid@gmail.com>
2022-11-18 22:58:06 +0100

    Add interface to update player high score

---
+++ project/src/Player/PlayerCamera.gd
@@ -0,0 +1,5 @@
+extends GridMap
+
+onready var world_grid = get_parent().get_node("Ground")
+
+var high_score: int
---
+++ project/src/UI/StaticUI.gd
@@ -0,0 +1,8 @@
+extends Control
+
+onready var _player = get_parent().get_node("PlayerCamera")
+onready var ui_high_score = get_node("HighScoreLabel")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	ui_high_score.text = "High Score: " + str(_player.high_score)
