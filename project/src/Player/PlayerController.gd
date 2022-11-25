1f1222b63da3f254c4f2aec724cf4ac03dafbbdd
loteque <andrewandroid@gmail.com>
2022-11-26 00:47:03 +0100

    add methods and test them

--- project/src/Player/PlayerController.gd
+++ project/src/Player/PlayerController.gd
@@ -8,4 +8,20 @@ onready var placement_grid: GridMap = get_node(placement_grid_path)
 
 # PLAYER DATA
 export (float, 0, 100) var movement_speed = 10.0
+
 var high_score: int
+var cursor_pos: int
+
+#PLAYER CONTROLLER METHODS
+func get_cursor_position() -> Vector3:
+	return(Vector3(0,0,0))
+
+func get_gridmesh_item_under_cursor() -> Dictionary:
+	return ({"Placeholder Dictionary": "This is a future dictionary of gridmesh items under the cursor",
+			"Item Name": "MeshLib Index"})
+			
+func _ready():
+	# smoke tests
+	print("PlayerController interface smoke tests:")
+	print(get_cursor_position())
+	print(get_gridmesh_item_under_cursor())
