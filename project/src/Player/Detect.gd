ae00e460194d8c4e8b3247eecf800d7cb15348c8
loteque <andrewandroid@gmail.com>
2022-11-26 02:32:43 +0100

    update select and detect interfaces

--- project/src/Player/Detect.gd
+++ project/src/Player/Detect.gd
@@ -4,6 +4,6 @@ export (NodePath) onready var cursor_path = ".."
 
 onready var cursor = get_node(cursor_path)
 
-func detect_first_soul():
+func is_first_soul():
 	if cursor.get_world_grid_cell_item() == 0:
-		Signals.emit_signal("souls_detected", 1)
+		return true
--- project/src/Player/Select.gd
+++ project/src/Player/Select.gd
@@ -5,5 +5,6 @@ export (NodePath) onready var cursor_path = ".."
 onready var cursor = get_node(cursor_path) 
 
 func _on_select_first_soul() -> void:
-	if Input.is_action_just_pressed("ui_accept") and cursor.detect_soul() == 0:
+	if Input.is_action_just_pressed("ui_accept") and cursor.detect.is_first_soul():
+		Signals.emit_signal("souls_detected", 1)
 		cursor.set_world_grid_cell_item(-1)
