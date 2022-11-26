304d16b954b78be266aff62f2988279af5954be0
loteque <andrewandroid@gmail.com>
2022-11-26 02:39:02 +0100

    add signal frist_soul_collected: Frist_soul_collected

--- project/src/Player/Select.gd
+++ project/src/Player/Select.gd
@@ -6,5 +6,5 @@ onready var cursor = get_node(cursor_path)
 
 func _on_select_first_soul() -> void:
 	if Input.is_action_just_pressed("ui_accept") and cursor.detect.is_first_soul():
-		Signals.emit_signal("souls_detected", 1)
+		Signals.emit_signal("first_soul_collected")
 		cursor.set_world_grid_cell_item(-1)
--- project/src/Signals.gd
+++ project/src/Signals.gd
@@ -1,3 +1,4 @@
 extends Node
 
 signal souls_detected(num_souls)
+signal first_soul_collected()
