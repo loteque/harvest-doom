f6344d47ea48177556566ee9dc81d3c84d6e2944
Ace Luke <acelukemusic@gmail.com>
2022-12-08 16:19:56 -0800

    Implement Attractor Plant SFX
    Attractor Plant SFX should play when the Attractor Plant is planted after being selected from the radial menu

--- project/src/Audio/PlantAttractor.gd
+++ project/src/Audio/PlantAttractor.gd
@@ -1,16 +1,9 @@
 extends Node
 
+onready var sfx = get_node("AudioStreamPlayer")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Signals.connect("attractor_plant_planted", self, "_on_attractor_plant_planted")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_attractor_plant_planted():
+	sfx.play()
